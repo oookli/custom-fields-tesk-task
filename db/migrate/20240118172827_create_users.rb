ec54c5c4880346d188b52bcba530b18a4e67a25e
Ilko Fedak <oklika@gmail.com>
2024-01-20 19:32:10 +0200

    basic configuration for rubocop

--- db/migrate/20240118172827_create_users.rb
+++ db/migrate/20240118172827_create_users.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-
       t.timestamps
     end
   end
--- db/migrate/20240118181053_add_custom_fields_to_user.rb
+++ db/migrate/20240118181053_add_custom_fields_to_user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddCustomFieldsToUser < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :custom_fields, :jsonb
